af2f6a711533181da39e34626b8bafd99c9bd965
ryandi purba <purbaryandi@gmail.com>
2021-11-12 13:52:34 +0700

    finish project and fix bugs

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "assets/css/app.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
+import Details from "pages/Details";
+import Cart from "pages/Cart";
+import Congratulation from "pages/Congratulation";
+import NotFound from "pages/NotFound";
+
+import Provider from "helpers/hooks/useGlobalContext";
+function App() {
+  return (
+    <Provider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/categories/:idc" component={Details} />
+          <Route path="/categories/:idc/products/:idp" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/congratulation" component={Congratulation} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
